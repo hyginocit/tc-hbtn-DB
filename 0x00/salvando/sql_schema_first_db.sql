97d7ea51f0b4bb00cc249e6ef1aec0e3dc9aa5e3
hyginocit <gnieves@ciandt.com>
2022-06-28 15:32:40 -0300

    first schema

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE CLIENTE
+(id integer primary key autoincrement,
+nome varchar(80) not null,
+email varchar(40) not null,
+telefone INTEGER);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
