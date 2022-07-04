e2679391682833b3964192699d0f71b1603aac48
hyginocit <gnieves@ciandt.com>
2022-07-04 09:50:59 -0300

    jpa

---
+++ jpa_hibernate/src/main/java/entities/Pessoa.java
@@ -0,0 +1,83 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private Integer idade;
+
+    private String cpf;
+
+    @Temporal(TemporalType.DATE)
+    private Calendar dataNascimento;
+
+    public Pessoa(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Calendar getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Calendar dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
